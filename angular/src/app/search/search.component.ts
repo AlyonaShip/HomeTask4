91aee084c026ad84dd686fe7b2dac9f002f2dba1
AlyonaShip <AlyonaShip>
2021-07-29 02:28:47 +0300

    change search component

--- angular/src/app/search/search.component.ts
+++ angular/src/app/search/search.component.ts
@@ -13,14 +13,10 @@ export class SearchComponent implements OnInit {
   public listOfPrograms!: Array<Program>;
   public searchData!: string; 
   private params = new HttpParams();
+  public isFirstTap = true;
   
   constructor(private http: HttpClient) { 
-    this.http.get<any>(environment.baseUrl).subscribe(result =>{
-      this.listOfPrograms = result;
-    },
-    error =>{
-      console.log(error);
-    })
+    this.FirstTap();    
    }
 
   ngOnInit(): void {
@@ -28,12 +24,27 @@ export class SearchComponent implements OnInit {
 
     getData(searchData: string){
       this.params.set('searchData', this.searchData)      
-      return this.http.get<any>(environment.baseUrl, {params: {searchData: this.searchData}}).subscribe(result => {
-        this.listOfPrograms = result
+      this.http.get<any>(environment.baseUrl, {params: {searchData: this.searchData}}).subscribe(result => {
+        
+      if(this.isFirstTap === false){
+        this.listOfPrograms = result;
+      }
+      else{
+        this.listOfPrograms = result.slice(0, 10);
+      }
       },
       error =>{
         console.log(error);
       });
   }
 
+  public FirstTap(){
+    this.http.get<any>(environment.baseUrl).subscribe(result =>{
+      this.listOfPrograms = result.slice(0, 10);
+    },
+    error =>{
+      console.log(error);
+    })
+    this.isFirstTap = false;
+  }
 }
