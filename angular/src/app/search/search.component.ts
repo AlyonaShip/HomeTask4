0e29085f3ffcc64edc624abb9255307803bd98bc
AlyonaShip <AlyonaShip>
2021-08-03 09:31:31 +0300

    changed search request

--- angular/src/app/search/search.component.ts
+++ angular/src/app/search/search.component.ts
@@ -11,40 +11,46 @@ import { Program } from '../models/program';
 export class SearchComponent implements OnInit {
   
   public listOfPrograms!: Array<Program>;
-  public searchData!: string; 
+  public searchData = ''; 
+  private headers = new HttpHeaders();
   private params = new HttpParams();
-  public isFirstTap = true;
+  public isFirstTap = true;  
   
   constructor(private http: HttpClient) { 
     this.FirstTap();    
    }
 
   ngOnInit(): void {
+    
+  } 
+
+  getData(searchData: string){
+    this.headers.append('Content-Type', 'application/json');
+    this.params.set("searchData", this.searchData);   
+    if(searchData && searchData.length >= 3){
+      setTimeout(()=>{                           
+        this.http.get<any>(environment.baseUrl, {params: {searchData: this.searchData}}).subscribe(result => {        
+        if(this.isFirstTap === false){                                 
+          this.listOfPrograms = result;             
+        }
+        else{
+          this.listOfPrograms = result.slice(0, 10);
+        }
+        },
+        error =>{
+          console.log(error);
+        });
+      }, 3000);      
+    }
   }
 
-    getData(searchData: string){
-      this.params.set('searchData', this.searchData)      
-      this.http.get<any>(environment.baseUrl, {params: {searchData: this.searchData}}).subscribe(result => {
-        
-      if(this.isFirstTap === false){
-        this.listOfPrograms = result;
-      }
-      else{
-        this.listOfPrograms = result.slice(0, 10);
-      }
-      },
-      error =>{
-        console.log(error);
-      });
-  }
-
-  public FirstTap(){
+  public FirstTap(){    
     this.http.get<any>(environment.baseUrl).subscribe(result =>{
       this.listOfPrograms = result.slice(0, 10);
     },
     error =>{
       console.log(error);
-    })
-    this.isFirstTap = false;
+    })  
+    this.isFirstTap = false;     
   }
 }
